9a3bb8ab3d03eb089fe6462fd9f47552b13e7776
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-07-23 07:11:57 +0000

    created post listed view

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,4 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,20 +10,13 @@ class Post(models.Model):
     """
 
     image_filter_choices = [
-        ('1977', '1977'),
-        ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'),
-        ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'),
-        ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'),
-        ('normal', 'Normal'),
-        ('nashville', 'Nashville'),
-        ('rise', 'Rise'),
-        ('toaster', 'Toaster'),
-        ('valencia', 'Valencia'),
-        ('walden', 'Walden'),
-        ('xpro2', 'X-pro II')
+        ('_1977', '1977'), ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
+        ('nashville', 'Nashville'), ('rise', 'Rise'),
+        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
+        ('walden', 'Walden'), ('xpro2', 'X-pro II')
     ]
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -36,11 +27,12 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_jah4w4', blank=True)
 
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
+
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return f'{self.id} {self.title}'
-        image_filter = models.CharField(
-            max_length=32, choices=image_filter_choices, default='normal'
-        )
---
+++ posts/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from posts import views
+urlpatterns = [
+    path('posts/', views.PostList.as_view()),
+]
