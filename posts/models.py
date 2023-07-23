5a21868be7e9f477f4104e81bedb755f85f2cb24
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-07-23 06:40:45 +0000

    added image filter field

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+
+# Create your models here.
+from django.db import models
 from django.contrib.auth.models import User
 
 
@@ -9,28 +12,35 @@ class Post(models.Model):
     """
 
     image_filter_choices = [
-        ('_1977', '1977'), ('brannan', 'Brannan'),
-        ('earlybird', 'Earlybird'), ('hudson', 'Hudson'),
-        ('inkwell', 'Inkwell'), ('lofi', 'Lo-Fi'),
-        ('kelvin', 'Kelvin'), ('normal', 'Normal'),
-        ('nashville', 'Nashville'), ('rise', 'Rise'),
-        ('toaster', 'Toaster'), ('valencia', 'Valencia'),
-        ('walden', 'Walden'), ('xpro2', 'X-pro II')
+        ('1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
     ]
+
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rgq6aq', blank=True)
-
-    image_filter = models.CharField(
-        max_length=32, choices=image_filter_choices, default='normal'
-    )
+        upload_to='images/', default='../default_post_jah4w4', blank=True)
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return f'{self.id} {self.title}'
+        image_filter = models.CharField(
+            max_length=32, choices=image_filter_choices, default='normal'
+        )
