a2b599187094cd56a64580ac858ebc7916330810
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-07-24 14:47:38 +0000

    connect to sql

--- drf/settings.py
+++ drf/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
 import os
 import dj_database_url
 
@@ -135,12 +134,14 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 
 if 'DEV' in os.environ:
     DATABASES = {
-        'default': ({
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        } if 'DEV' in os.environ else dj_database_url.parse(
-            os.environ.get('DATABASE_URL')
-        ))
+        }
+    }
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
