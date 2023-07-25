5b72a8a80ade89575d1f1efcedfbe9d0b3990032
banafshaahmadyar <ahmadyarbanafsha@gmail.com>
2023-07-25 08:17:17 +0000

    changed procfile

--- drf/urls.py
+++ drf/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route, logout_route
+from .views import root_route
 
 urlpatterns = [
     path('', root_route),
@@ -26,9 +26,6 @@ urlpatterns = [
     path('', include('comments.urls')),
     path('', include('likes.urls')),
     path('', include('followers.urls')),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # our logout route has to be above the default one to be matched first
-    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 
